a881e154ed57efa10817e0151d33fb1b448d3c08
Hannu Lyytinen <hannux@ssrc.tii.ae>
2021-12-01 13:26:35 +0200

    Add seL4 accelerator
    Signed-off-by: Hannu Lyytinen <hannux@ssrc.tii.ae>

--- hw/pci-host/gpex.c
+++ hw/pci-host/gpex.c
@@ -47,9 +47,13 @@ struct GPEXIrq {
     int irq_num;
 };
 
+void sel4_set_irq(unsigned int irq, bool);
+
 static void gpex_set_irq(void *opaque, int irq_num, int level)
 {
     GPEXHost *s = opaque;
+
+    sel4_set_irq(irq_num, !!level);
 
     qemu_set_irq(s->irq[irq_num].irq, level);
 }
