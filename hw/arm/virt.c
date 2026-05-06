tiiuae/qemu-sel4-virtio

Add interrupt controller for seL4
Signed-off-by: Markku Ahvenjärvi <markku.ahvenjarvi@unikie.com>

hw/arm/virt.c
@@ -49,6 +49,7 @@
 #include "sysemu/tpm.h"
 #include "sysemu/kvm.h"
 #include "sysemu/hvf.h"
+#include "sysemu/sel4.h"
 #include "hw/loader.h"
 #include "qapi/error.h"
 #include "qemu/bitops.h"
@@ -834,6 +835,16 @@ static void create_gic(VirtMachineState *vms, MemoryRegion *mem)
     } else if (vms->gic_version == VIRT_GIC_VERSION_2) {
         create_v2m(vms);
     }
+}
+
+static void create_sel4_intc(VirtMachineState *vms)
+{
+    vms->gic = qdev_new("sel4-intc");
+    qdev_prop_set_uint32(vms->gic, "num-irqs", NUM_IRQS);
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(vms->gic), &error_fatal);
+
+    fdt_add_gic_node(vms);
+    create_v2m(vms);
 }
 
 static void create_uart(const VirtMachineState *vms, int uart,
@@ -2222,7 +2233,11 @@ static void machvirt_init(MachineState *machine)
 
     virt_flash_fdt(vms, sysmem, secure_sysmem ?: sysmem);
 
-    create_gic(vms, sysmem);
+    if (!sel4_enabled()) {
+        create_gic(vms, sysmem);
+    } else {
+        create_sel4_intc(vms);
+    }
 
     virt_cpu_post_init(vms, sysmem);
 

virtioso/virtioso-qemu

hw/arm/virt.c
@@ -50,6 +50,7 @@
 #include "system/kvm.h"
 #include "system/hvf.h"
 #include "system/qtest.h"
+#include "system/sel4.h"
 #include "system/system.h"
 #include "hw/loader.h"
 #include "qapi/error.h"
@@ -962,6 +963,16 @@ static void create_gic(VirtMachineState *vms, MemoryRegion *mem)
     } else if (vms->gic_version == VIRT_GIC_VERSION_2) {
         create_v2m(vms);
     }
+}
+
+static void create_sel4_intc(VirtMachineState *vms)
+{
+    vms->gic = qdev_new("sel4-intc");
+    qdev_prop_set_uint32(vms->gic, "num-irqs", NUM_IRQS);
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(vms->gic), &error_fatal);
+
+    fdt_add_gic_node(vms);
+    create_v2m(vms);
 }
 
 static void create_uart(const VirtMachineState *vms, int uart,
@@ -2462,7 +2473,11 @@ static void machvirt_init(MachineState *machine)
 
     virt_flash_fdt(vms, sysmem, secure_sysmem ?: sysmem);
 
-    create_gic(vms, sysmem);
+    if (!sel4_enabled()) {
+        create_gic(vms, sysmem);
+    } else {
+        create_sel4_intc(vms);
+    }
 
     virt_post_cpus_gic_realized(vms, sysmem);
 